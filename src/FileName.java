6f06dd34e50b8db48161b04a383e612363fae88f
vishnuprabhu1995 <145844513+vishnuprabhu1995@users.noreply.github.com>
2023-11-10 09:34:48 +0530

    Update FileName.java

--- src/FileName.java
+++ src/FileName.java
@@ -1,1 +1,32 @@
-abc
+public class FileName {
+
+	public static void main(String[] args) {
+		
+		List<String> list = new ArrayList<>();
+		list.add("2");
+		list.add("54");
+		list.add("44");
+		list.add("1");
+		
+		int arr[] = new int [list.size()];
+		int q=0;
+		for(String str : list) {
+			arr[q] = Integer.parseInt(str);
+			q++;
+		}
+		
+		int temp;
+		
+		for(int i=0; i<arr.length;i++) {
+			for(int j=0;j<arr.length;j++) {
+				if(arr[i] < arr[j]) {
+					temp = arr[j];
+					arr[j] = arr[i];
+					arr[i] = temp;
+				}
+			}
+		}
+		
+		System.out.println(Arrays.toString(arr));
+		
+	}
